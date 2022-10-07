b7a3b918c7899fb368bb277bbea0865a2403cbd0
Zaptross <zaptross@outlook.com>
2022-10-07 22:03:24 +1000

    chore: some api helper functions

---
+++ lib/utils/validation.ts
@@ -0,0 +1,13 @@
+const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+export function isEmailValid(email: string) {
+  return emailRegex.test(email);
+}
+
+const uuidRegex =
+  /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+
+export function isValidUuidV4(uuid: string) {
+  return uuidRegex.test(uuid);
+}
